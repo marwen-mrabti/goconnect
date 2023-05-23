d035cfed9dc81aac44aa038283274ef6c6ba0245
marwen <marwen.mrabti.me@gmail.com>
2023-05-23 18:59:10 +0000

    add cors

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const cors=require("cors")
 
 //import routes
 const users = require('./routes/api/users');
@@ -16,6 +17,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
+app.use(cors())
 
 //passport config
 require('./security/passport')(passport);
